d8211bf2673607c40385815bb76a9dc21083b935
Artur Signell <artur@vaadin.com>
2019-11-25 15:58:45 +0200

    Add GitHub link

--- frontend/my-view.ts
+++ frontend/my-view.ts
@@ -2,21 +2,25 @@ import { vaadinV, vaadinH } from "./icons/vaadin-icons";
 
 import "@vaadin/vaadin-button";
 import { LitElement, html, customElement } from "lit-element";
+import "github-corner";
 
 @customElement("my-view")
 export class MyView extends LitElement {
   render() {
     return html`
-      <vaadin-button
-        ><span style="fill: blue; color: blue;"
-          >${vaadinV} Hello</span
-        ></vaadin-button
-      >
-      <vaadin-button
-        ><span style="fill: red; color: red;"
-          >${vaadinH} Hello</span
-        ></vaadin-button
-      >
+      <github-corner>
+        <a href="https://github.com/Artur-/hello-icons"></a>
+        <vaadin-button
+          ><span style="fill: blue; color: blue;"
+            >${vaadinV} Hello</span
+          ></vaadin-button
+        >
+        <vaadin-button
+          ><span style="fill: red; color: red;"
+            >${vaadinH} Hello</span
+          ></vaadin-button
+        >
+      </github-corner>
     `;
   }
 }
